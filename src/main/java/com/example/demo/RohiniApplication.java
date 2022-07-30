97cab10ad60064282d11cdfb4056d7b94052319e
Rohini <rohinishelke@gmail.com>
2022-07-30 17:59:12 +0530

    one added

---
+++ src/main/java/com/example/demo/RohiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RohiniApplication.class, args);
+	}
+
+}
